418f988b61fb15f999d377a1911d511bfc4d4013
mayank <sismmt09@gmail.com>
2020-10-22 10:29:54 +0530

    changed datatype for isLegendary

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
 df = pd.read_csv('./resources/pokemon_alopez247.csv')
-print(df.columns)
+# print(df.columns)
+df = df[['Name','isLegendary','Generation', 'Type_1', 'Type_2', 'HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def', 'Speed','Color','Egg_Group_1','Height_m','Weight_kg','Body_Style']]
+# print(df.columns)
+df['isLegendary'] = df['isLegendary'].astype(int)
+print(df)
